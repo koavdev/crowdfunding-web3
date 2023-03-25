da2589746ccec2d3684929452273296ee99ef2ec
koavdev <koavdev@gmail.com>
2023-03-25 11:24:51 -0300

    c2 - created formData object

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,7 +1,9 @@
+import React, { useContext } from 'react';
 import { AiFillPlayCircle } from 'react-icons/ai';
 import { SiEthereum } from 'react-icons/si';
-import { BsInfo, BsInfoCircle } from 'react-icons/bs';
+import { BsInfoCircle } from 'react-icons/bs';
 
+import { TransactionContext } from '../context/TransactionContext';
 import { Loader } from './';
 
 const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
@@ -10,7 +12,7 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
     <input
         placeholder={placeholder}
         type={type}
-        setp="0.0001"
+        step="0.0001"
         value={value}
         onChange={(e) => handleChange(e, name)}
         className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
@@ -19,13 +21,17 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
 
 const Welcome = () => {
 
-    const connectWallet = () => {
+    const { connectWallet, currentAccount, formData, sendTransaction, handleChange } = useContext(TransactionContext);
 
-    }
+    const handleSubmit = (e) => {
+        const { addressTo, amount, keyword, message } = formData;
 
-    const handleSubmit = () => {
+        e.preventDefault();
 
-    }
+        if(!addressTo || !amount || !keyword || !message) return;
+
+        sendTransaction();
+    };
 
     return (
         <div className='flex w-full justify-center items-center'>
@@ -37,13 +43,17 @@ const Welcome = () => {
                     <p className='text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base'>
                         Explore the crypto world. Buy and sell cryptocurriences easily on Krypto.
                     </p>
-                    <button
-                        type="button"
-                        onClick={connectWallet}
-                        className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
-                    >
-                        <p className='text-white text-base font-semibold'>Connect Wallet</p>
-                    </button>
+                    {!currentAccount && (
+                        <button
+                            type="button"
+                            onClick={connectWallet}
+                            className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
+                        >
+                            <AiFillPlayCircle className="text-white mr-2" />
+                            <p className='text-white text-base font-semibold'>Connect Wallet</p>
+                        </button>
+                    )}
+                    
 
                     <div className='grid sm:grid-cols-3 grid-cols-2 w-full mt-10'>
                         <div className={`rounded-tl-2xl ${commonStyles}`}>Reliability</div>
@@ -76,10 +86,10 @@ const Welcome = () => {
                     </div>                    
                 </div>
                 <div className='p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism'>
-                    <Input placeholder="Address To" name="addressTo" type="text" handleChange={() => {}}/>
-                    <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={() => {}}/>
-                    <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={() => {}}/>
-                    <Input placeholder="Enter message" name="message" type="text" handleChange={() => {}}/>
+                    <Input placeholder="Address To" name="addressTo" type="text" handleChange={handleChange}/>
+                    <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={handleChange}/>
+                    <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={handleChange}/>
+                    <Input placeholder="Enter message" name="message" type="text" handleChange={handleChange}/>
 
                     <div className='h-[1px] w-full bg-gray-400 my-2'/>
 
---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transactions.json';
+
+export const contractABI = abi.abi;
+export const contractAddress = '0x74630D957623BeDb74e2a552e85D103bA91bA18C';
